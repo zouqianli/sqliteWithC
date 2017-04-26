477a60f203283f18f720b68159278aae4655b58f
zouqianli <1004248149@qq.com>
2017-04-26 08:01:37 +0800

    Initial Commit

---
+++ sqliteWithC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sqliteWithC
+//
+//  Created by 邹前立 on 2017/4/26.
+//  Copyright © 2017年 邹前立. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
